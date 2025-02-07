618aa8e4d8378d4d12545915c6bfedfb7ff50f50
Higor Hotz Vasconcelos <highotz.hotz@hotmail.com>
2025-02-06 21:08:43 -0300

    Processo de deploy da lambda

---
+++ terraform/main.tf
@@ -0,0 +1,100 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Role da Lambda
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Política de permissões da Lambda
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "lambda_access_policy"
+  description = "Permissões para Lambda acessar S3, DynamoDB e SES"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem"
+        ],
+        Resource = "arn:aws:dynamodb:us-east-1:617932910341:table/Videos"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        Resource = [
+          "arn:aws:s3:::meu-bucket-input/*",
+          "arn:aws:s3:::meu-bucket-output/*"
+        ]
+      },
+      {
+        Effect = "Allow",
+        Action = "ses:SendEmail",
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "arn:aws:logs:us-east-1:617932910341:log-group:/aws/lambda/video_processor:*"
+      }
+    ]
+  })
+}
+
+# Anexa a política ao Role
+resource "aws_iam_role_policy_attachment" "attach_lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+# Função Lambda
+resource "aws_lambda_function" "video_processor" {
+  function_name    = "video_processor"
+  role            = aws_iam_role.lambda_role.arn
+  handler        = "lambda_function.lambda_handler"
+  runtime        = "python3.8"
+  timeout        = 60
+  memory_size    = 512
+  s3_bucket      = "app-processa-video"
+  s3_key         = "video_processor.zip"
+
+  environment {
+    variables = {
+      TABLE_NAME = "Videos"
+    }
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.attach_lambda_policy]
+}
+
+# Permissão para a Lambda ser acionada por eventos do SQS (se necessário)
+resource "aws_lambda_permission" "allow_sqs" {
+  statement_id  = "AllowExecutionFromSQS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.video_processor.function_name
+  principal     = "sqs.amazonaws.com"
+  source_arn    = "arn:aws:sqs:us-east-1:617932910341:videos-queue"
+}
---
+++ terraform/varables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "lambda_bucket" {
+  description = "S3 bucket para armazenar o código da Lambda"
+  default     = "app-processa-video"
+}
