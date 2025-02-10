cb1c68ed58863121015781a88a7b302d2db51b10
Paulo Junior <paulo.avelinojunior@outlook.com>
2025-02-09 23:36:25 -0300

    feat:ajuste

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,3 +63,40 @@ resource "aws_lambda_permission" "allow_sqs" {
   principal     = "sqs.amazonaws.com"
   source_arn    = "arn:aws:sqs:us-east-1:980029326297:videos-queue"
 }
+# Buscar a Lambda existente, se ela já existir
+data "aws_lambda_function" "existing_lambda" {
+  function_name = "video_processor"
+}
+
+# Criar Lambda apenas se não existir
+resource "aws_lambda_function" "video_processor" {
+  count         = try(length(data.aws_lambda_function.existing_lambda.arn), 0) > 0 ? 0 : 1
+  function_name = "video_processor"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+  timeout       = 60
+  memory_size   = 512
+  s3_bucket     = "processa-video-infra"
+  s3_key        = "video_processor.zip"
+
+  environment {
+    variables = {
+      TABLE_NAME = "Videos"
+    }
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.attach_lambda_policy, aws_iam_role_policy_attachment.attach_lambda_sqs_policy]
+}
+
+# Permissão para a Lambda ser acionada por eventos do SQS
+resource "aws_lambda_permission" "allow_sqs" {
+  statement_id  = "AllowExecutionFromSQS"
+  action        = "lambda:InvokeFunction"
+  function_name = coalesce(
+    try(data.aws_lambda_function.existing_lambda.function_name, ""),
+    try(length(aws_lambda_function.video_processor) > 0 ? aws_lambda_function.video_processor[0].function_name : "")
+  )
+  principal     = "sqs.amazonaws.com"
+  source_arn    = data.aws_sqs_queue.existing_video_queue.arn
+}
